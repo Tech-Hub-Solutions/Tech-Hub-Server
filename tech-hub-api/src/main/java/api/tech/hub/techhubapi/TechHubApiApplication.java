b0bcbe919cc63bfdafa83b3ccffd3a634cd5b82c
MRESENDESILVA <mateus.rsilva@sptech.school>
2023-09-23 20:08:29 -0300

    feat: reestruturação do back-end.

---
+++ tech-hub-api/src/main/java/api/tech/hub/techhubapi/TechHubApiApplication.java
@@ -0,0 +1,13 @@
+package api.tech.hub.techhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechHubApiApplication.class, args);
+	}
+
+}
---
+++ tech-hub-api/src/test/java/api/tech/hub/techhubapi/TechHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.tech.hub.techhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
