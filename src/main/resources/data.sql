7d2eff753da94741689729fba8bb8703bcba3076
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-10-18 21:40:18 -0300

    refactor: atualização senha de teste

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO Perfil (experiencia) VALUES ('experiencia');
 
 INSERT INTO Usuario (perfil_id,nome, email, senha, numero_cadastro_pessoa, pais, funcao, is_ativo)
-VALUES (1,'Murilo', 'murilo@hotmail.com', '$2a$10$ZMM5Jes4mEKhZoRbu4lafuk5BSL0OMwbtYvcfq2LaxXthfBfdxWDq', '26828654000100', 'Brasil', 'dev', 1);
+VALUES (1,'Murilo', 'murilo@hotmail.com', '$2a$10$feRbfBjRcoDMaXXkBuc06u3lA5ng3CMkPXsHapLKXtezQYh4UURDq', '26828654000100', 'Brasil', 'dev', 1);
 
 INSERT INTO Usuario (nome, email, senha, numero_cadastro_pessoa, pais, funcao, is_ativo)
 VALUES ('Leonardo', 'leo@hotmail.com', '$2a$10$ZMM5Jes4mEKhZoRbu4lafuk5BSL0OMwbtYvcfq2LaxXthfBfdxWDq', '89138182000175', 'Brasil', 'empresa', 1);
