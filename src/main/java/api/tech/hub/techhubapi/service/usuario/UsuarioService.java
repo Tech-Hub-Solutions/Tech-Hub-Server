bef8e5c2737b36d30d1f15c0f1c5d6924f4d685d
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-10-22 20:24:19 -0300

    fix: algoritmo de ordenação

--- src/main/java/api/tech/hub/techhubapi/service/usuario/UsuarioService.java
+++ src/main/java/api/tech/hub/techhubapi/service/usuario/UsuarioService.java
@@ -72,7 +72,7 @@ public class UsuarioService {
         Perfil perfilUsuario = this.perfilRepository.save(new Perfil());
         Usuario usuarioSalvo = this.usuarioRepository.save(validado);
 
-        this.perfilRepository.atualizarUsuarioDoPerfil(perfilUsuario.getId(),usuarioSalvo);
+        this.perfilRepository.atualizarUsuarioDoPerfil(perfilUsuario.getId(), usuarioSalvo);
         this.usuarioRepository.atualizarPerfilDoUsuario(usuarioSalvo.getId(), perfilUsuario);
 
         return usuarioSalvo;
@@ -106,26 +106,31 @@ public class UsuarioService {
     }
 
     public ListaObj<UsuarioDetalhadoDto> listar() {
-        ListaObj<UsuarioDetalhadoDto> usuarios = new ListaObj<>(10);
+        ListaObj<UsuarioDetalhadoDto> usuarios = new ListaObj<>(40);
 
 
         for (Usuario u : this.usuarioRepository.findAll()) {
             usuarios.adiciona(new UsuarioDetalhadoDto(u));
         }
 
-        // Ordernar por nome
+        // OrdernarPorNome
         for (int i = 0; i < usuarios.getTamanho() - 1; i++) {
-            UsuarioDetalhadoDto menorNome = usuarios.getElemento(i);
+            int menor = i;
+            String menorNome = usuarios.getElemento(menor).nome();
+
             for (int j = i + 1; j < usuarios.getTamanho(); j++) {
-                UsuarioDetalhadoDto usuario = usuarios.getElemento(j);
-                if (usuario.nome().compareTo(menorNome.nome()) < 0) {
-                    menorNome = usuario;
+                String nomeAtual = usuarios.getElemento(j).nome();
+                if (nomeAtual.compareTo(menorNome) < 0) {
+                    menor = j;
+                    menorNome = usuarios.getElemento(menor).nome();
                 }
             }
-            UsuarioDetalhadoDto aux = usuarios.getElemento(i);
-            usuarios.setElemento(i, menorNome);
-            usuarios.setElemento(usuarios.getTamanho() - 1, aux);
 
+            if (menor != i) {
+                UsuarioDetalhadoDto aux = usuarios.getElemento(i);
+                usuarios.setElemento(i, usuarios.getElemento(menor));
+                usuarios.setElemento(menor, aux);
+            }
         }
 
 
@@ -133,7 +138,7 @@ public class UsuarioService {
     }
 
     public ListaObj<UsuarioConversaDto> listarTeste() {
-        ListaObj<UsuarioConversaDto> usuarios = new ListaObj<>(10);
+        ListaObj<UsuarioConversaDto> usuarios = new ListaObj<>(40);
 
 
         for (Usuario u : this.usuarioRepository.findAll()) {
