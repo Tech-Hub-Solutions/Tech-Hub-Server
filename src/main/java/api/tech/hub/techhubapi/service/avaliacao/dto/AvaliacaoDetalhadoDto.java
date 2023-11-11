07a809a871986bdc19b20b4e3241c8cead932298
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-11-11 20:10:09 -0300

    feat: trazer todas informações do usuário

--- src/main/java/api/tech/hub/techhubapi/service/avaliacao/dto/AvaliacaoDetalhadoDto.java
+++ src/main/java/api/tech/hub/techhubapi/service/avaliacao/dto/AvaliacaoDetalhadoDto.java
@@ -2,12 +2,27 @@ package api.tech.hub.techhubapi.service.avaliacao.dto;
 
 import api.tech.hub.techhubapi.entity.perfil.Avaliacao;
 import api.tech.hub.techhubapi.entity.perfil.Perfil;
+import api.tech.hub.techhubapi.service.arquivo.ArquivoService;
+import api.tech.hub.techhubapi.service.arquivo.TipoArquivo;
 import jakarta.persistence.ManyToOne;
 
-public record AvaliacaoDetalhadoDto (int id, String nomePerfil, String avaliador, String comentario, Integer qtdEstrela) {
+public record AvaliacaoDetalhadoDto(
+        int id,
+        String nomePerfil,
+        String avaliador,
+        String urlFotoPerfil,
+        String pais,
+        String comentario,
+        Integer qtdEstrela
+) {
 
-    public AvaliacaoDetalhadoDto (Avaliacao avaliacao) {
-        this(avaliacao.getId(), avaliacao.getPerfil().getUsuario().getNome(), avaliacao.getAvaliador().getUsuario().getNome(),
+    public AvaliacaoDetalhadoDto(Avaliacao avaliacao) {
+        this(
+                avaliacao.getId(),
+                avaliacao.getPerfil().getUsuario().getNome(),
+                avaliacao.getAvaliador().getUsuario().getNome(),
+                ArquivoService.criarUrlFoto(avaliacao.getAvaliador(), TipoArquivo.PERFIL),
+                avaliacao.getAvaliador().getUsuario().getPais(),
                 avaliacao.getComentario(), avaliacao.getQtdEstrela());
     }
 }
