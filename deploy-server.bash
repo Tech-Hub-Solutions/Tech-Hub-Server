3a20ee09d3903b05222ee1e14b0362e977beabb0
MuriloSBarbosa <murilo.barbosa@sptech.school>
2024-05-23 20:49:44 -0300

    feat: adding deploy with ssh

--- deploy-server.bash
+++ deploy-server.bash
@@ -1,20 +1,20 @@
 # var with key name
 KEY_NAME="key.pem"
 # array with ip addresses
-IP_ADDRESSES=("10.0.0.0" "10.0.0.0")
+IP_ADDRESSES=("10.0.0.247" "10.0.0.230")
 # default user
 USER="ubuntu"
 
 # loop through the array
 for ip in "${IP_ADDRESSES[@]}"
 do
-  echo "Deploying to $ip"
+  # jump line
+  printf "\n\n\nDeploying to $ip \n\n"
+  echo "$(date) => Deploying to $ip" >> deploy-server.log
   # ssh into the server
   sudo ssh -i /home/$USER/.ssh/$KEY_NAME $USER@$ip '
     docker compose down
     docker compose pull
     docker compose up -d
   '
-  # add in log file
-  echo "Deployed to $ip" >> deploy-server.log
 done
